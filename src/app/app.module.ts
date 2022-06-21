afd80cf084d3c6fe3cd8eea209fc66f8e1b92efd
Daan Keun <daan.keun@entelect.co.za>
2022-06-21 17:34:13 +0200

    Add angular firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 
 @NgModule({
     declarations: [AppComponent, DashboardComponent],
-    imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        AppRoutingModule,
+        MatToolbarModule,
+        MatIconModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideFunctions(() => getFunctions()),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+    firebase: {
+        projectId: 'fine-app-2e3ea',
+        appId: '1:699345184044:web:957cc3109df33b242c7889',
+        storageBucket: 'fine-app-2e3ea.appspot.com',
+        locationId: 'europe-west',
+        apiKey: 'AIzaSyDwDW8hGh9WlENgdlN1ENW_4l_RnNAAeLE',
+        authDomain: 'fine-app-2e3ea.firebaseapp.com',
+        messagingSenderId: '699345184044',
+        measurementId: 'G-LW6PJRFG6V',
+    },
     production: true,
 };
